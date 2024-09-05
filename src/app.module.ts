4da80ab86361a025cc116aa556691ab9eed46c5c
Amin7775 <amin.dpi24@gmail.com>
2024-09-06 04:15:27 +0600

    setup modules for buyer,seller,product,order,seller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SellerModule } from './seller/seller.module';
+import { BuyerModule } from './buyer/buyer.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [],
+  imports: [SellerModule, BuyerModule, ProductModule, OrderModule, ReviewModule],
 })
 export class AppModule {}
