a20f90111fe3be500fe8324925303f7b194d7fa1
Amin7775 <amin.dpi24@gmail.com>
2024-09-06 06:34:28 +0600

    feat : seller register logic completed

--- src/seller/seller.service.ts
+++ src/seller/seller.service.ts
@@ -1,15 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
 import { RegisterSellerDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class SellerService {
   constructor(private prisma: PrismaService) {}
 
-  register(dto: RegisterSellerDto) {
-    const hash = argon.hash(dto.password);
-    console.log(hash);
-    // return { msg: 'This is Register' };
+  async register(dto: RegisterSellerDto) {
+    // generate the pw hash
+    const hash = await argon.hash(dto.password);
+
+    try {
+      // save new user in db
+      const user = await this.prisma.seller.create({
+        data: {
+          email: dto.email,
+          hash,
+          name: dto.name,
+        },
+      });
+      // to stop showing hash
+      delete user.hash;
+      // return the user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken');
+        }
+      }
+      throw error;
+    }
   }
 }
