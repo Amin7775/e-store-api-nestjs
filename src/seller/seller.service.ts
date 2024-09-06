de269d5f42c05e7093c464f3efc043fc4e942d93
Amin7775 <amin.dpi24@gmail.com>
2024-09-06 06:22:08 +0600

    Hashing user password with argon

--- src/seller/seller.service.ts
+++ src/seller/seller.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { RegisterSellerDto } from './dto';
 
 @Injectable()
 export class SellerService {
   constructor(private prisma: PrismaService) {}
 
-  register(dto) {
-    console.log(dto);
+  register(dto: RegisterSellerDto) {
+    const hash = argon.hash(dto.password);
+    console.log(hash);
     // return { msg: 'This is Register' };
   }
 }
